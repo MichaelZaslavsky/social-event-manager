dfe51443766a4204989a359b6a6acf9dbb036989
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-05-12 01:15:45 +0300

    Add Swagger documentation

---
+++ SocialEventManager/src/SocialEventManager.Shared/Constants/ApiConstants.cs
@@ -0,0 +1,9 @@
+namespace SocialEventManager.Shared.Constants
+{
+    public static class ApiConstants
+    {
+        public const string Swagger = "swagger";
+        public const string SocialEventManagerApi = "Social Event Manager API";
+        public const string FirstVersion = "v1";
+    }
+}
