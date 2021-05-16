ce467576d8a82b1ae522020358dd2efdf8c4d791
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-05-16 19:54:37 +0300

    Categorize tests

--- SocialEventManager/tests/SocialEventManager.Tests/UnitTests/ExtensionTests/StringExtensionsTests.cs
+++ SocialEventManager/tests/SocialEventManager.Tests/UnitTests/ExtensionTests/StringExtensionsTests.cs
@@ -1,10 +1,14 @@
 using System;
+using SocialEventManager.Shared.Constants;
 using SocialEventManager.Shared.Extensions;
 using SocialEventManager.Tests.DataMembers.Common;
 using Xunit;
+using Xunit.Categories;
 
 namespace SocialEventManager.Tests.UnitTests.ExtensionTests
 {
+    [UnitTest]
+    [Category(CategoryConstants.Extensions)]
     public class StringExtensionsTests
     {
         [Theory]
