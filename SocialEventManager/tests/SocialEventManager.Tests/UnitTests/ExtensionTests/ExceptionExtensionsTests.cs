3c23d2681fdd924b2becea63ff355b76d996fe40
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-05-20 03:24:33 +0300

    Add exception extension tests

--- SocialEventManager/tests/SocialEventManager.Tests/UnitTests/ExtensionTests/ExceptionExtensionsTests.cs
+++ SocialEventManager/tests/SocialEventManager.Tests/UnitTests/ExtensionTests/ExceptionExtensionsTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using SocialEventManager.Shared.Constants;
 using SocialEventManager.Shared.Extensions;
 using SocialEventManager.Tests.Common.DataMembers;
@@ -11,6 +12,14 @@ namespace SocialEventManager.Tests.UnitTests.ExtensionTests
     [Category(CategoryConstants.Extensions)]
     public class ExceptionExtensionsTests
     {
+        [Theory]
+        [MemberData(nameof(ExceptionData.ExceptionDataForHttpStatusAndTitle), MemberType = typeof(ExceptionData))]
+        public void ToHttpStatusCodeAndTitle(Exception ex, (HttpStatusCode, string) expectedResult)
+        {
+            (HttpStatusCode, string) actualResult = ex.ToHttpStatusCodeAndTitle();
+            Assert.Equal(expectedResult, actualResult);
+        }
+
         [Theory]
         [MemberData(nameof(ExceptionData.CriticalExceptionsData), MemberType = typeof(ExceptionData))]
         public void IsCritical(Exception ex, bool expectedResult)
