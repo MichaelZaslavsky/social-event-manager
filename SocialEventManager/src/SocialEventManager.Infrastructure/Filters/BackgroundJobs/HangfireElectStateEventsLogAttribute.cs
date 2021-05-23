7441986884870b7ee053b1925f80f904d1ec7108
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-05-23 13:45:39 +0300

    Add Hangfire filters and inject ElecState in startup

---
+++ SocialEventManager/src/SocialEventManager.Infrastructure/Filters/BackgroundJobs/HangfireElectStateEventsLogAttribute.cs
@@ -0,0 +1,22 @@
+using Hangfire.Common;
+using Hangfire.Logging;
+using Hangfire.States;
+
+namespace SocialEventManager.Infrastructure.Filters.BackgroundJobs
+{
+    public class HangfireElectStateEventsLogAttribute : JobFilterAttribute, IElectStateFilter
+    {
+        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+
+        public void OnStateElection(ElectStateContext context)
+        {
+            if (context.CandidateState is FailedState failedState)
+            {
+                Logger.WarnFormat(
+                    "IElectStateFilter: Job `{0}` has been failed due to an exception `{1}`",
+                    context.BackgroundJob.Id,
+                    failedState.Exception);
+            }
+        }
+    }
+}
---
+++ SocialEventManager/src/SocialEventManager.Infrastructure/Filters/BackgroundJobs/HangfireServerEventsLogAttribute.cs
@@ -0,0 +1,21 @@
+using Hangfire.Common;
+using Hangfire.Logging;
+using Hangfire.Server;
+
+namespace SocialEventManager.Infrastructure.Filters.BackgroundJobs
+{
+    public class HangfireServerEventsLogAttribute : JobFilterAttribute, IServerFilter
+    {
+        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+
+        public void OnPerforming(PerformingContext context)
+        {
+            Logger.InfoFormat("IServerFilter: Starting to perform job `{0}`", context.BackgroundJob.Id);
+        }
+
+        public void OnPerformed(PerformedContext context)
+        {
+            Logger.InfoFormat("IServerFilter: Job `{0}` has been performed", context.BackgroundJob.Id);
+        }
+    }
+}
