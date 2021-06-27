9c4660ef12ba15912f8c28ca996e32b6b080c132
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-06-27 11:36:11 +0300

    Remove temporary users feature

--- SocialEventManager/shared/SocialEventManager.Shared/Constants/TableNameConstants.cs
+++ SocialEventManager/shared/SocialEventManager.Shared/Constants/TableNameConstants.cs
@@ -5,7 +5,6 @@ namespace SocialEventManager.Shared.Constants
         public const string Changelog = "Changelog";
         public const string Accounts = "dbo.Accounts";
         public const string Roles = "dbo.Roles";
-        public const string Users = "dbo.Users";
         public const string UserRoles = "dbo.UserRoles";
         public const string UserClaims = "dbo.UserClaims";
     }
