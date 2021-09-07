ff8afc731033fa5b5eac512562222024ac168a95
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-09-08 00:41:44 +0300

    Add XML media type to the global API configuration

--- SocialEventManager/src/SocialEventManager.API/Configurations/ResponseTypeConfigurations.cs
+++ SocialEventManager/src/SocialEventManager.API/Configurations/ResponseTypeConfigurations.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SocialEventManager.Shared.Constants;
 
 namespace SocialEventManager.API.Configurations
 {
+    /// <summary>
+    /// Represents API response type configurations.
+    /// </summary>
     public static class ResponseTypeConfigurations
     {
+        /// <summary>
+        /// Configuring global response type attributes.
+        /// </summary>
+        /// <param name="config">The options to set its filters.</param>
         public static void ConfigureGlobalResponseTypeAttributes(this MvcOptions config)
         {
             config.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest));
@@ -13,6 +21,7 @@ namespace SocialEventManager.API.Configurations
             config.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status429TooManyRequests));
             config.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status500InternalServerError));
             config.Filters.Add(new ProducesDefaultResponseTypeAttribute());
+            config.Filters.Add(new ProducesAttribute(MediaTypeConstants.ApplicationJson, MediaTypeConstants.ApplicationXml));
         }
     }
 }
--- SocialEventManager/src/SocialEventManager.Infrastructure/Middleware/ApiExceptionMiddleware.cs
+++ SocialEventManager/src/SocialEventManager.Infrastructure/Middleware/ApiExceptionMiddleware.cs
@@ -55,7 +55,7 @@ namespace SocialEventManager.Infrastructure.Middleware
             Log.Write(level, ex, $"{ExceptionConstants.AnErrorOccurred}: {message} -- {error.Id}.");
 
             string result = JsonConvert.SerializeObject(error);
-            context.Response.ContentType = MimeTypeConstants.ApplicationJson;
+            context.Response.ContentType = MediaTypeConstants.ApplicationJson;
             context.Response.StatusCode = (int)httpStatusCode;
 
             return context.Response.WriteAsync(result);
