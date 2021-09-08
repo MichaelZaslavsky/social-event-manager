cf1fe48d64362a83651713f312d831fe06161a45
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-09-08 13:00:00 +0300

    Add API versioning to Swagger

--- SocialEventManager/shared/SocialEventManager.Shared/Constants/ApiConstants.cs
+++ SocialEventManager/shared/SocialEventManager.Shared/Constants/ApiConstants.cs
@@ -3,8 +3,9 @@ namespace SocialEventManager.Shared.Constants
     public static class ApiConstants
     {
         public const string Swagger = "swagger";
-        public const string SocialEventManagerApi = "Social Event Manager API";
-        public const string FirstVersion = "v1";
+        public const string SocialEventManagerApi = "SocialEventManagerAPI";
+        public const string SocialEventManagerApiTitle = "Social Event Manager API";
+        public const string SocialEventManagerApiDescription = "Access to REST APIs";
         public const string AllowAll = "AllowAll";
         public const string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
         public const string AppSettings = "appsettings";
@@ -18,5 +19,7 @@ namespace SocialEventManager.Shared.Constants
         public const string ApiVersion = "x-api-version";
         public const string Version = "version";
         public const string Ver = "ver";
+        public const string License = "GPL-3.0 License";
+        public const string LicenseUrl = "https://github.com/MichaelZaslavsky/social-event-manager/blob/master/LICENSE";
     }
 }
---
+++ SocialEventManager/shared/SocialEventManager.Shared/Constants/ContactConstants.cs
@@ -0,0 +1,9 @@
+namespace SocialEventManager.Shared.Constants
+{
+    public static class ContactConstants
+    {
+        public const string Name = "Michael Zaslavsky";
+        public const string Email = "MichaelZaslavsky2@gmail.com";
+        public const string Url = "https://www.linkedin.com/in/michael-zaslavsky/";
+    }
+}
