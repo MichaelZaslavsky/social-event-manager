d8fbc37a6b91d97c81c2fef8159511641037537a
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-11-08 22:49:02 +0200

    Fix failing test - HealthCheck_Should_Return_Ok_Status_Code

--- SocialEventManager/src/SocialEventManager.API/Program.cs
+++ SocialEventManager/src/SocialEventManager.API/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Enrichers.AspnetcoreHttpcontext;
 using SocialEventManager.API.Utilities.Extensions;
+using SocialEventManager.Infrastructure.Migrations;
 using SocialEventManager.Shared.Constants;
 
 namespace SocialEventManager.API
@@ -24,6 +26,7 @@ namespace SocialEventManager.API
                 .AddJsonFile(ApiConstants.AppSettingsJson, optional: false, reloadOnChange: true)
                 .AddJsonFile($"{ApiConstants.AppSettings}.{EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables()
+                .AddUserSecrets<Startup>()
                 .Build();
         }
 
@@ -32,7 +35,10 @@ namespace SocialEventManager.API
             try
             {
                 IHost host = CreateHostBuilder(args).Build();
+
                 Log.Information(ApiConstants.StartingHost);
+                Task.Run(() => new DbMigrations(Configuration).Migrate(EnvironmentName)).GetAwaiter().GetResult();
+
                 host.Run();
             }
             catch (Exception ex)
@@ -48,13 +54,6 @@ namespace SocialEventManager.API
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostContext, builder) =>
-                {
-                    if (hostContext.HostingEnvironment.IsDevelopment())
-                    {
-                        builder.AddUserSecrets<Startup>();
-                    }
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureKestrel(options => options.AddServerHeader = false)
