7ac190f0ef8f1440ead674f44684d9472b743fa3
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2021-11-09 00:57:26 +0200

    Define locale when manipulating a string

--- SocialEventManager/tests/SocialEventManager.Tests/IntegrationTests/Data/RolesData.cs
+++ SocialEventManager/tests/SocialEventManager.Tests/IntegrationTests/Data/RolesData.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using SocialEventManager.DAL.Entities;
 using SocialEventManager.Shared.Helpers;
 
@@ -18,7 +19,7 @@ namespace SocialEventManager.Tests.IntegrationTests.Data
                     Id = Guid.NewGuid(),
                     ConcurrencyStamp = RandomGeneratorHelpers.GenerateRandomValue(),
                     Name = RandomGeneratorHelpers.GenerateRandomValue(),
-                    NormalizedName = name.ToUpper(),
+                    NormalizedName = name.ToUpper(CultureInfo.InvariantCulture),
                 },
             };
         }
