20f31020d223761fe3da1a8e4c4003676e9ed251
Michael Zaslavsky <michael.zaslavsky@graduway.com>
2022-06-05 14:57:58 +0300

    Interpolate strings in constant variables

--- SocialEventManager/shared/SocialEventManager.Shared/Constants/ApiPathConstants.cs
+++ SocialEventManager/shared/SocialEventManager.Shared/Constants/ApiPathConstants.cs
@@ -5,12 +5,13 @@ public static class ApiPathConstants
     public const string ApiController = Api + Controller;
     public const string Roles = Api + "roles";
     public const string Ready = "ready";
-    public const string HealthReady = "/" + Health + Ready;
-    public const string HealthLive = "/" + Health + "live";
+    public const string HealthReady = $"/{Health}/{Ready}";
+    public const string HealthLive = $"/{Health}/live";
+
     public const string ChatHub = "/chathub";
     public const string Hangfire = "/hangfire";
 
     private const string Api = "api/";
     private const string Controller = "[controller]";
-    private const string Health = "health/";
+    private const string Health = "health";
 }
