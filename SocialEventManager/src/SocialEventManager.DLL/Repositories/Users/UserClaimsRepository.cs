592e883d48e5ba80a77523607bb7163923da9dee
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-06-19 13:56:07 +0300

    Indent SQL commands

--- SocialEventManager/src/SocialEventManager.DLL/Repositories/Users/UserClaimsRepository.cs
+++ SocialEventManager/src/SocialEventManager.DLL/Repositories/Users/UserClaimsRepository.cs
@@ -19,10 +19,10 @@ public class UserClaimsRepository : GenericRepository<UserClaim>, IUserClaimsRep
     public async Task<IEnumerable<UserClaim>> GetUserClaims(string type, string value)
     {
         string sql = $@"
-                SELECT  UC.*
-                FROM    {TableNameConstants.UserClaims} UC
-                WHERE   UC.[Type] = @Type
-                        AND UC.[Value] = @Value;";
+            SELECT  UC.*
+            FROM    {TableNameConstants.UserClaims} UC
+            WHERE   UC.[Type] = @Type
+                    AND UC.[Value] = @Value;";
 
         return await _session.Connection.QueryAsync<UserClaim>(sql, new DynamicParameters(new { type, value }), _session.Transaction);
     }
@@ -32,17 +32,17 @@ public class UserClaimsRepository : GenericRepository<UserClaim>, IUserClaimsRep
         string userClaimsJson = JsonConvert.SerializeObject(userClaims);
 
         string sql = $@"
-                DELETE  UC
-	            FROM    {TableNameConstants.UserClaims} UC
-                        INNER JOIN OPENJSON(@UserClaimsJson)
-	                    WITH
-                        (
-                            UserId	UNIQUEIDENTIFIER    N'$.UserId',
-                            [Type]  NVARCHAR(255)       N'$.Type',
-                            [Value] NVARCHAR(255)       N'$.Value'
-                        ) AS UCJ ON UC.UserId = UCJ.UserId
-                            AND UC.[Type] = UCJ.[Type]
-                            AND UC.[Value] = UCJ.[Value];";
+            DELETE  UC
+	        FROM    {TableNameConstants.UserClaims} UC
+                    INNER JOIN OPENJSON(@UserClaimsJson)
+	                WITH
+                    (
+                        UserId	UNIQUEIDENTIFIER    N'$.UserId',
+                        [Type]  NVARCHAR(255)       N'$.Type',
+                        [Value] NVARCHAR(255)       N'$.Value'
+                    ) AS UCJ ON UC.UserId = UCJ.UserId
+                        AND UC.[Type] = UCJ.[Type]
+                        AND UC.[Value] = UCJ.[Value];";
 
         return await _session.Connection.ExecuteAsync(sql, new DynamicParameters(new { userClaimsJson }), _session.Transaction) > 0;
     }
