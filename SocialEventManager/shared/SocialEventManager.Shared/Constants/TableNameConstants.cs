347e7b3601fa01fe7ddeb71cc605588907a81b09
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-06-19 14:17:13 +0300

    Update SchemaType table to be plural

--- SocialEventManager/shared/SocialEventManager.Shared/Constants/TableNameConstants.cs
+++ SocialEventManager/shared/SocialEventManager.Shared/Constants/TableNameConstants.cs
@@ -4,6 +4,7 @@ public static class TableNameConstants
 {
     public const string Accounts = $"{SchemaConstants.Default}.{nameof(Accounts)}";
     public const string Changelog = nameof(Changelog);
+    public const string SchemaTypes = nameof(SchemaTypes);
     public const string Roles = $"{SchemaConstants.Default}.{nameof(Roles)}";
     public const string UserClaims = $"{SchemaConstants.Default}.{nameof(UserClaims)}";
     public const string UserRoles = $"{SchemaConstants.Default}.{nameof(UserRoles)}";
