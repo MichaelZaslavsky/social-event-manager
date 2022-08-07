9043292062b60e744ddb816c4386e21838f47048
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-08-07 12:08:12 +0300

    Fix test names according to the naming convention

--- SocialEventManager/tests/SocialEventManager.Tests/UnitTests/ExtensionTests/ExceptionExtensionsTests.cs
+++ SocialEventManager/tests/SocialEventManager.Tests/UnitTests/ExtensionTests/ExceptionExtensionsTests.cs
@@ -14,7 +14,7 @@ public class ExceptionExtensionsTests
 {
     [Theory]
     [MemberData(nameof(ExceptionData.ExceptionDataForHttpStatusAndTitle), MemberType = typeof(ExceptionData))]
-    public void ToHttpStatusCodeAndTitle_Should_ReturnCorrectResult_WhenCalled(Exception? ex, (HttpStatusCode, string) expectedResult)
+    public void ToHttpStatusCodeAndTitle_Should_ReturnCorrectResult_When_Called(Exception? ex, (HttpStatusCode, string) expectedResult)
     {
         (HttpStatusCode, string) actualResult = ex.ToHttpStatusCodeAndTitle();
         actualResult.Should().Be(expectedResult);
@@ -22,7 +22,7 @@ public class ExceptionExtensionsTests
 
     [Theory]
     [MemberData(nameof(ExceptionData.CriticalExceptionsData), MemberType = typeof(ExceptionData))]
-    public void IsCritical_Should_ReturnCorrectResult_WhenCalled(Exception? ex, bool expectedResult)
+    public void IsCritical_Should_ReturnCorrectResult_When_Called(Exception? ex, bool expectedResult)
     {
         bool actualResult = ex.IsCritical();
         actualResult.Should().Be(expectedResult);
