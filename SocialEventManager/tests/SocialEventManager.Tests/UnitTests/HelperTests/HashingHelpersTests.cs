4ac53f2a959425c8deeac3ff4074e07301118a3c
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-08-24 23:49:48 +0300

    Add hashing helpers tests

---
+++ SocialEventManager/tests/SocialEventManager.Tests/UnitTests/HelperTests/HashingHelpersTests.cs
@@ -0,0 +1,23 @@
+using FluentAssertions;
+using SocialEventManager.Shared.Constants;
+using SocialEventManager.Shared.Helpers;
+using SocialEventManager.Tests.Common.Constants;
+using Xunit;
+using Xunit.Categories;
+
+namespace SocialEventManager.Tests.UnitTests.HelperTests;
+
+[UnitTest]
+[Category(CategoryConstants.Helpers)]
+public class HashingHelpersTests
+{
+    [Theory]
+    [InlineData(TestConstants.SomeText, TestConstants.LoremIpsum)]
+    public void RSHash_Should_ReturnHashedValue_When_Called(string value, string otherValue)
+    {
+        int actualValue = HashingHelpers.RSHash(value);
+        int actualOtherValue = HashingHelpers.RSHash(otherValue);
+
+        actualValue.Should().NotBe(actualOtherValue);
+    }
+}
