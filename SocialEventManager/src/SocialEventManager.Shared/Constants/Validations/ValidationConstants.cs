e9c88883c056b47b92efe1398d96a260d77ec73b
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-09-10 16:07:36 +0300

    Add length tests for contact us feature

--- SocialEventManager/src/SocialEventManager.Shared/Constants/Validations/ValidationConstants.cs
+++ SocialEventManager/src/SocialEventManager.Shared/Constants/Validations/ValidationConstants.cs
@@ -8,23 +8,13 @@ public static class ValidationConstants
     public const string NotAValidIdentifier = "Not a valid identifier.";
     public const string TheFieldMustNotHaveTheDefaultValue = "The {0} field must not have the default value.";
 
-    public static string LessThanMinimumLength(string fieldName, int minLength) =>
-        $"The field {fieldName} must be a string or array type with a minimum length of '{minLength}'.";
-
     public static string InvalidEmail(string fieldName) => $"The {fieldName} field is not a valid e-mail address.";
 
     public static string TheFieldIsRequired(string fieldName) => $"The {fieldName} field is required.";
 
-    public static string FieldMaximumLength(string fieldName, int length) => FieldLength(fieldName, length, true);
-
-    public static string FieldMinimumLength(string fieldName, int length) => FieldLength(fieldName, length, false);
+    public static string LengthNotInRange(string filedName, int minimum, int maximum) =>
+        $"The field {filedName} must be a string with a minimum length of {minimum} and a maximum length of {maximum}.";
 
     public static string CouldNotFindThisView(string viewPath, string searchedLocations) =>
         $"Could not find this view: {viewPath}. Searched locations:\n{searchedLocations}";
-
-    private static string FieldLength(string fieldName, int length, bool isMaximum)
-    {
-        string maxOrMin = isMaximum ? "maximum" : "minimum";
-        return $"The field {fieldName} must be a string or array type with a {maxOrMin} length of '{length}'.";
-    }
 }
