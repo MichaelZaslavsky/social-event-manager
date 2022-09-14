da556ca0489631a8cd18046b91bd6264e8de9e75
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-09-14 23:12:39 +0300

    Add exception tests

---
+++ SocialEventManager/tests/SocialEventManager.Tests/UnitTests/UtilityTests/BadRequestExceptionTests.cs
@@ -0,0 +1,34 @@
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using SocialEventManager.Shared.Constants;
+using SocialEventManager.Shared.Exceptions;
+using SocialEventManager.Tests.Common.Constants;
+using Xunit;
+using Xunit.Categories;
+
+namespace SocialEventManager.Tests.UnitTests.UtilityTests;
+
+[UnitTest]
+[Category(CategoryConstants.Utilities)]
+public class BadRequestExceptionTests
+{
+    [Theory]
+    [AutoData]
+    public void InitBadRequestException_Should_Succeed_When_MessageIsValid(string message)
+    {
+        BadRequestException ex = new(message);
+        ex.Message.Should().Be(message);
+        ex.HResult.Should().Be(TestConstants.ExceptionHResult);
+        ex.InnerException.Should().BeNull();
+    }
+
+    [Theory]
+    [AutoData]
+    public void InitBadRequestException_Should_Succeed_When_MessageAndInnerExceptionAreValid(string message, Exception innerException)
+    {
+        BadRequestException ex = new(message, innerException);
+        ex.Message.Should().Be(message);
+        ex.HResult.Should().Be(TestConstants.ExceptionHResult);
+        ex.InnerException.Should().Be(innerException);
+    }
+}
