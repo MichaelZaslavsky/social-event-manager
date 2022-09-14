0532fe3572b3b01e71937224bf3f0404c4f03058
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-09-14 20:46:33 +0300

    Add service exceptions wrapper infrastructure

---
+++ SocialEventManager/src/SocialEventManager.Shared/Extensions/ResultExtensions.cs
@@ -0,0 +1,33 @@
+using LanguageExt.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialEventManager.Shared.Exceptions;
+
+namespace SocialEventManager.Shared.Extensions;
+
+using FluentValidation;
+
+public static class ResultExtensions
+{
+    public static IActionResult ToOk<TResult, TContract>(this Result<TResult> result, Func<TResult, TContract> mapper)
+    {
+        return result.Match(
+            obj =>
+            {
+                TContract response = mapper(obj);
+                return new OkObjectResult(response);
+            }, exception => GetResult(exception));
+    }
+
+    private static IActionResult GetResult(Exception exception)
+    {
+        return exception switch
+        {
+            ValidationException ex => new BadRequestObjectResult(ex.Message),
+            UnauthorizedAccessException ex => new UnauthorizedObjectResult(ex.Message),
+            NotFoundException ex => new NotFoundObjectResult(ex.Message),
+            UnprocessableEntityException ex => new UnprocessableEntityObjectResult(ex.Message),
+            _ => new StatusCodeResult(StatusCodes.Status500InternalServerError),
+        };
+    }
+}
