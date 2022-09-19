07d459501e54eb8b0c6842470ea82c0ff1c3f62b
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-09-19 18:53:35 +0300

    Add Scope Information tests

---
+++ SocialEventManager/tests/SocialEventManager.Tests/UnitTests/Models/ScopeInformationTests.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using FluentAssertions;
+using SocialEventManager.Infrastructure.Loggers;
+using SocialEventManager.Shared.Constants;
+using Xunit;
+
+namespace SocialEventManager.Tests.UnitTests.Models;
+
+public class ScopeInformationTests
+{
+    [Fact]
+    public void Init_Should_Succeed_When_Called()
+    {
+        ScopeInformation scopeInfo = new();
+
+        scopeInfo.HostScopeInfo.Keys.Should().Contain(GlobalConstants.MachineName);
+        scopeInfo.HostScopeInfo[GlobalConstants.MachineName].Should().Be(Environment.MachineName);
+
+        scopeInfo.HostScopeInfo.Keys.Should().Contain(GlobalConstants.EntryPoint);
+        scopeInfo.HostScopeInfo[GlobalConstants.EntryPoint].Should().Be(Assembly.GetEntryAssembly()?.GetName().Name!);
+    }
+}
