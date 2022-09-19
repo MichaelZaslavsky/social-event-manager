61c15cae95a4df88f3b3e2f029bf9d43ea212f98
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-09-19 19:11:58 +0300

    Add Message Helpers tests

---
+++ SocialEventManager/tests/SocialEventManager.Tests/UnitTests/HelperTests/MessageHelpersTests.cs
@@ -0,0 +1,26 @@
+using FluentAssertions;
+using SocialEventManager.Shared.Constants;
+using SocialEventManager.Shared.Helpers;
+using Xunit;
+using Xunit.Categories;
+
+namespace SocialEventManager.Tests.UnitTests.HelperTests;
+
+[UnitTest]
+[Category(CategoryConstants.Helpers)]
+public class MessageHelpersTests
+{
+    [Fact]
+    public void BuildRequestMessage_Should_ReturnNull_When_HttpRequestIsNull()
+    {
+        string? message = MessageHelpers.BuildRequestMessage(null!);
+        message.Should().BeNull();
+    }
+
+    [Fact]
+    public void BuildResponseMessage_Should_ReturnNull_When_ApiErrorIsNull()
+    {
+        string? message = MessageHelpers.BuildResponseMessage(null!);
+        message.Should().BeNull();
+    }
+}
