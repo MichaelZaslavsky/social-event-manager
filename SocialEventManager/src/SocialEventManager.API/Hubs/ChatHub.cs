b0dabacc9b4ace1686e8c4d3c38fd438a5cafaad
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-09-19 19:08:16 +0300

    Exclude SignalR from code coverage as they are not yet in use

--- SocialEventManager/src/SocialEventManager.API/Hubs/ChatHub.cs
+++ SocialEventManager/src/SocialEventManager.API/Hubs/ChatHub.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.SignalR;
 using Serilog;
 using SocialEventManager.Shared.Constants;
 
 namespace SocialEventManager.API.Hubs;
 
+[ExcludeFromCodeCoverage]
 public sealed class ChatHub : Hub
 {
     public async Task SendMessage(Guid userId, string message)
--- SocialEventManager/src/SocialEventManager.API/Hubs/ChatHubLogFilter.cs
+++ SocialEventManager/src/SocialEventManager.API/Hubs/ChatHubLogFilter.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
 using Serilog;
 
 namespace SocialEventManager.API.Hubs;
 
+[ExcludeFromCodeCoverage]
 public sealed class ChatHubLogFilter : IHubFilter
 {
     public async ValueTask<object?> InvokeMethodAsync(HubInvocationContext invocationContext, Func<HubInvocationContext, ValueTask<object?>> next)
