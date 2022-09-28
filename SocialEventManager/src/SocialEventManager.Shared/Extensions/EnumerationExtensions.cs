ac41a63e64d17719ffa8cbf5008ece03657acf26
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-09-28 11:48:25 +0300

    Fix warning REFL010
    Prefer the generic extension method GetCustomAttribute<DescriptionAttribute>

--- SocialEventManager/src/SocialEventManager.Shared/Extensions/EnumerationExtensions.cs
+++ SocialEventManager/src/SocialEventManager.Shared/Extensions/EnumerationExtensions.cs
@@ -8,10 +8,7 @@ public static class EnumerationExtensions
     public static string GetDescription(this Enum value)
     {
         MemberInfo? enumMember = value.GetType().GetMember(value.ToString()).FirstOrDefault();
-
-        DescriptionAttribute? descriptionAttribute = enumMember is null
-            ? default
-            : enumMember.GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute;
+        DescriptionAttribute? descriptionAttribute = enumMember?.GetCustomAttribute<DescriptionAttribute>();
 
         return descriptionAttribute is null
             ? value.ToString()
