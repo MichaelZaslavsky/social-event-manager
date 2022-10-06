b86243008bd489340a0f883257eff52900e13569
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-10-06 18:50:26 +0300

    Add email constants

---
+++ SocialEventManager/src/SocialEventManager.Shared/Constants/EmailConstants.cs
@@ -0,0 +1,8 @@
+namespace SocialEventManager.Shared.Constants;
+
+public static class EmailConstants
+{
+    public const string LayoutPath = "~/EmailTemplates/_Layout.cshtml";
+    public const string ForgotPasswordSubject = "Password Reset";
+    public const string VerifyEmailSubject = "Verify email address";
+}
