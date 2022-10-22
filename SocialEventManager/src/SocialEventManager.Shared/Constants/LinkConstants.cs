dcc6efbbdbcba6e59c2fd88c7476ffdab491e043
MichaelZaslavsky <Michael.Zaslavsky@graduway.com>
2022-10-22 17:23:04 +0300

    Upgrate from .NET 6.0 to .NET 7.0
    Upgrate projects to .NET 7.0
    
    .NET 7 includes many improvements including massive performance improvements. It comes along with C# 11.
    
    For more info:
    - [.NET 7 Features](https://blog.dotnetsafer.com/new-official-net-7-features-released-now-faster-and-lighter/)
    - [What's new in C# 11](https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-11)
    - [All C# 11 features](https://blog.dotnetsafer.com/csharp-11-is-coming-revealing-all-csharp-11-features/)
    
    Upgrade code from to .NET 7.0
    
    Update dotnet links to .NET 7.0
    
    Update Docker dotnet to .NET 7.0
    
    Update CI/CD workflow to .NET 7.0

--- SocialEventManager/src/SocialEventManager.Shared/Constants/LinkConstants.cs
+++ SocialEventManager/src/SocialEventManager.Shared/Constants/LinkConstants.cs
@@ -5,12 +5,12 @@ public static class LinkConstants
     public const string BadRequestException = $"{WikiHttpStatusUrl}#400";
     public const string NotFoundException = $"{WikiHttpStatusUrl}#404";
     public const string UnprocessableEntity = $"{WikiHttpStatusUrl}#422";
-    public const string NullReferenceException = $"{MicrosoftApiUrl}system.nullreferenceexception?view=net-6.0";
-    public const string ArgumentNullException = $"{MicrosoftApiUrl}system.argumentnullexception.-ctor?view=net-6.0";
-    public const string ArgumentException = $"{MicrosoftApiUrl}system.argumentexception?view=net-6.0";
-    public const string TimeoutException = $"{MicrosoftApiUrl}system.timeoutexception?view=net-6.0";
-    public const string SqlException = $"{MicrosoftApiUrl}system.data.sqlclient.sqlexception?view=dotnet-plat-ext-6.0";
-    public const string Exception = $"{MicrosoftApiUrl}system.exception?view=net-6.0";
+    public const string NullReferenceException = $"{MicrosoftApiUrl}system.nullreferenceexception?view=net-7.0";
+    public const string ArgumentNullException = $"{MicrosoftApiUrl}system.argumentnullexception.-ctor?view=net-7.0";
+    public const string ArgumentException = $"{MicrosoftApiUrl}system.argumentexception?view=net-7.0";
+    public const string TimeoutException = $"{MicrosoftApiUrl}system.timeoutexception?view=net-7.0";
+    public const string SqlException = $"{MicrosoftApiUrl}system.data.sqlclient.sqlexception?view=dotnet-plat-ext-7.0";
+    public const string Exception = $"{MicrosoftApiUrl}system.exception?view=net-7.0";
 
     private const string WikiHttpStatusUrl = "https://en.wikipedia.org/wiki/List_of_HTTP_status_codes";
     private const string MicrosoftApiUrl = "https://docs.microsoft.com/en-us/dotnet/api/";
